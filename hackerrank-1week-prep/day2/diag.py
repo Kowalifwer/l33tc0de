e728b608167ecf51bba44d2cdf50241a95a0a99a
Artem Grechushkin <artem.grechushkin@mail.ru>
2023-11-22 23:10:59 +0000

    minor change

--- hackerrank-1week-prep/day2/diag.py
+++ hackerrank-1week-prep/day2/diag.py
@@ -69,8 +69,8 @@ import sys
 #
 # Complete the 'diagonalDifference' function below.
 #
-# The function is expected to return an INTEGER.
-# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+# The function is expected to return an INTEGER
+# The function accepts 2D_INTEGER_ARRAY arr as parameter
 #
 
 def diagonalDifference(arr):
