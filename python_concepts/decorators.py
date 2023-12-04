ac9f47b284d9f7218fa35cd13a4beb83079e7faa
Artem Grechushkin <artem.grechushkin@mail.ru>
2023-12-04 21:25:33 +0000

    manually creating a decorator with @ and vanilla, to understand how it works

---
+++ python_concepts/decorators.py
@@ -0,0 +1,31 @@
+
+def surround_with_prints(func: callable):
+    def wrapper(*args, **kwargs):
+        if "extra" in kwargs: #showcasing that the decorator can access to all the params passed to the wrapped/decorated function.
+            print("WE GOT A CRAZY SETUP HERE") #this functionality comes from the wrapper, but depends on the wrapped params.
+
+        print("Print before")
+        print(args, kwargs)
+        func(*args, **kwargs)
+        print("Print after")
+    
+    return wrapper
+
+#wrapping with @
+@surround_with_prints
+def f1(a):
+    print(f"This is {a} function")
+
+def f2(a,b, **kwargs):
+
+    print(f"This is {a} {b} function")
+
+f1("CRAZY")
+
+#manual/OG wrapping
+f2_wrapped = surround_with_prints(f2) #initialize the wrapper, and pass the function to wrap inside of it
+f2_wrapped("one", "two", extra="please") #previously we defined the new function, with the wrapped functionality, that supports the original functions args. Now we can call it
+
+
+
+
