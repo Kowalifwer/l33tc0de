e299b9cd14a001a9cbf919055267d26e28ff0ea6
Artem Grechushkin <artem.grechushkin@mail.ru>
2023-12-06 16:47:43 +0000

    fibonacci with memoization

--- book/c1/recursive_practise.py
+++ book/c1/recursive_practise.py
@@ -13,3 +13,17 @@ def pow(x, n):
 
 print(factorial(4))
 print(pow(3,3))
+
+
+def fib(n, memo={}): #with memoization -> since each call splits the tree, this actually makes a difference
+    if n in memo:
+        return memo[n]
+    if n == 1 or n == 2:
+        return 1
+    else:
+        result = fib(n-1) + fib(n-2)
+        memo[n] = result
+        return result
+
+#note memoization does nothing if we have recursion without splitting the tree, like in the case of factorial.
+    
